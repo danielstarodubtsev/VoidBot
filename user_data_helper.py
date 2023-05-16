617ee53464d851b76dc31cfb030d22fe3410d0d7
DanTheMan <daniel.starodubtsev@gmail.com>
2023-05-16 09:16:25 +0300

    user data helper changed

--- user_data_helper.py
+++ user_data_helper.py
@@ -3,23 +3,14 @@ from random import choice
 
 input("Press enter to run")
 
-"""
 
 
 with open("user_data.json", "r") as json_file:
     data = json.load(json_file)
 
-symbols = "23456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
-
 for elem in data:
-    if data[elem]["commander"] == "None":
-        data[elem]["commander"] = None
+    data[elem]["event_points"] = 0
 
 with open("user_data.json", "w") as json_file:
     json_file.write(json.dumps(data, indent=2))
-"""
 
-print(True >= False)
-print(False >= False)
-print(True >= True)
-print(True <= False)
